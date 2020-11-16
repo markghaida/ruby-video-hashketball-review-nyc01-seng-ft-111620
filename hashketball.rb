386f8f3ef240e8e3c9410cd985b970fd8e949bc6
Mark G <markghaida@gmail.com>
2020-11-16 13:35:29 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,3 +1,4 @@
+require 'pry'
 # Write your code below game_hash
 
 def game_hash
@@ -128,3 +129,89 @@ def game_hash
 end
 
 # Write code here
+def num_points_scored (players_name)
+  #binding.pry
+  game_hash.each do |key, value|
+     #binding.pry
+    value.each do |k, v|
+       #binding.pry
+      if k == :players 
+         #binding.pry
+        game_hash[key][:players].each do |player|
+          player.each do |stat_key, stat_value|
+           #binding.pry
+            if stat_value == players_name
+               #binding.pry
+                return player[:points]
+                #binding.pry
+            end 
+          end  
+        end 
+      end 
+    end 
+  end 
+end 
+#Test is passing on Repl.it "Hashketball - most_points_scored"
+# def most_points_scored(game_hash)
+#   largest_num = 0
+#   largest_num_name = ""
+#   binding.pry
+#   game_hash.each do |key, value|
+#     binding.pry
+#     value.each do |k, v|
+#       binding.pry
+#       if k == :players 
+#         binding.pry
+#         game_hash[key][:players].each do |player|
+#           player.each do |stat_key, stat_value|
+#             binding.pry
+#             if player[:points] > largest_num
+#               largest_num = player[:points]
+#               largest_num_name = player[:player_name]
+#             end 
+#           end 
+#         end 
+#       end 
+#     end 
+#   end 
+# end 
+
+# def shoe_size(players_name)
+#   winning_team = []
+#   team1_points = 0
+#   team2_points = 0
+#   team1 = ""
+#   team2 = ""
+#   game_hash.each do |key, value|
+#   binding.pry
+#     value.each do |k, v|
+#       #binding.pry
+#       if k == :players 
+#         game_hash[key][:players].each do |player|
+#           #binding.pry
+#           player.each do |stat_key, stat_value|
+#             binding.pry
+#             if game_hash[key] == :home 
+#               binding.pry
+#               team1_points = team1_points + player[:points]
+#               team1_name = game_hash[key][:team_name]
+#             elsif game_hash[key] == :away
+#             binding.pry
+#               team2_points = team2_points + player[:points]
+#               team2_name = game_hash[key][:team_name]
+#             end 
+#           end 
+#         end 
+#       end 
+#     end 
+#   end 
+#   if team1_points > team2_points 
+#     binding.pry
+#         winning_team << team1[:team_name]
+#   elsif team2_points > team1_points 
+#   binding.pry
+#         winning_team << team2[:team_name]
+#   end 
+#   binding.pry
+#   puts winning_team[0]
+# end
--- spec/hashketball_spec.rb
+++ spec/hashketball_spec.rb
@@ -1,5 +1,5 @@
 require 'spec_helper'
-
+require 'pry'
 describe 'hashketball' do
   let(:alan_anderson) {
     { :player_name => "Alan Anderson", :number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1 }
